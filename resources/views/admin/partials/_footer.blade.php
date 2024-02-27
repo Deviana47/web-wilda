0d681ca5309788e08e8bbfd550483b6b6bb0b418
nurbaitilah7@gmail.com <nurbaitilah7@gmail.com>
2024-02-27 13:14:46 +0700

    ukk bonet

---
+++ resources/views/admin/partials/_footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="footer text-center text-muted">
+    All Rights Reserved by SMK Al Hafidz Leuwiliang. Designed and Developed by <a href="https://instagram.com/wilda0518" target="_blank">Wilda Revina</a>.
+</footer>
